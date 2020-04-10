e097104ddeca0fe0ce3437540bad51df32cc8f02
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-10 21:18:52 +0300

    Created the RecyclerView with the adapter

---
+++ app/src/main/java/com/slepnev/advancedrecyclerviewkotlin/ColorAdapter.kt
@@ -0,0 +1,33 @@
+package com.slepnev.advancedrecyclerviewkotlin
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_color.view.*
+
+class ColorAdapter(private val colors: List<ColorItem>) :
+        RecyclerView.Adapter<ColorAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorAdapter.ViewHolder {
+        context = parent.context
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_color, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = colors.size
+
+    override fun onBindViewHolder(holder: ColorAdapter.ViewHolder, position: Int) = holder.bind(colors[position])
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(colorItem: ColorItem) {
+            Glide.with(context).load(colorItem.getImageUrl()).into(itemView.ivColor)
+        }
+    }
+
+}
