a508de67280928488d61d4b41c0e706ef0205522
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-10 21:06:17 +0300

    Created the data class

---
+++ app/src/main/java/com/slepnev/advancedrecyclerviewkotlin/ColorItem.kt
@@ -0,0 +1,8 @@
+package com.slepnev.advancedrecyclerviewkotlin
+
+data class ColorItem(
+    var hex: String,
+    var name: String
+) {
+    fun getImageUrl() = "http://singlecolorimage.com/get/$hex/1080x1080"
+}
